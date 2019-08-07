e0ad64a0db983f89a5412f72197942a5d92ad06f
deepakrout <deepakrout@hotmail.com>
2019-08-07 14:16:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dr-angular-observable
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/dr-angular-observable)
---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {of, from } from 'rxjs'
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent implements OnInit {
+  name = 'Angular';
+
+  ngOnInit () {
+    of(1,2,3,4,5,6).subscribe(console.log)
+
+    from(['Hello','Hi', 'Namaste']).subscribe(
+      item => {console.log(`Next value is ${item}`)},
+      err => {console.log(`Error is ${err}`)},
+      ()=>{ console.log(`Stream Completed`) }
+    )
+  }
+}
